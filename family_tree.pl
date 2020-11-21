0821e8acb2f06bee9d91733a1f99346e43572b65
Kyle-C-CSU <71400517+Kyle-C-CSU@users.noreply.github.com>
2020-11-21 12:09:34 -0500

    Family_tree added
    updated p4 for q1

---
+++ family_tree.pl
@@ -0,0 +1,63 @@
+
+male(mushu).
+male(tangdee).
+male(X):-father(X,_).
+female(mulan).
+female(beumei).
+female(gugu).
+female(X):-mother(X,_).
+
+father(baba,mushu).
+father(baba,mulan).
+father(yeye,baba).
+father(yeye,gugu).
+father(yeye,susu).
+father(susu,tangdee).
+father(zengzufu,yeye).
+father(jojo,beumei).
+
+mother(mama,mushu).
+mother(mama,mulan).
+mother(popo,mama).
+mother(popo,jojo).
+
+/*
+Operations: 
+and ,
+or ;
+if :-
+not equivalent /=
+*/
+
+
+
+/*
+sibling: same mother 
+brother: male and  same mother
+aunt: 	 female and sibling of mother
+grandson: male descendant of fathers father
+descendant: mother or father 
+*/
+
+/*rules*/
+parent(X,Y):- father(X,Y).
+parent(X,Y):- mother(X,Y).
+grandparent(X,Z):-
+	parent(X,Y),  /*define relationship that grandparent is the parent of a parent*/
+	parent(Y,Z).  
+sibling(X,Y):- 
+	parent(Z,X),
+	parent(Z,Y),
+	X /= Y.		
+brother(X,Y):-
+	sibling(X,Y),  /*brother is a sibling*/
+	male(X).       /*brother is male*/
+aunt(X,Y):- 
+	parent(Z,Y),  /*neice parent is defined here*/
+	sibling(X,Z), /*aunt is sister with neice mother*/
+	female(X).    /*aunt is female*/
+grandson(X,Y):-
+	grandparent(Y,X), /*links relationship between grandparent*/
+	male(X).      /*grandson is male*/
+/*if person has relationship with parent then they are descendants of those parents*/
+descendant(X,Y):- parent(Y,X). 
